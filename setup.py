e0c48a10e58906a07730ffcebf49fdc13818654c
Colin Gaffney <cpgaffney@google.com>
2022-05-19 13:57:26 -0700

    Increase version to 0.0.1 to allow for release to work on supporting eventual clients.
    PiperOrigin-RevId: 449827028

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open('README.md') as fp:
 
 setuptools.setup(
     name='orbax',
-    version='0.0.0',
+    version='0.0.1',
     description='Orbax',
     long_description=_LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
