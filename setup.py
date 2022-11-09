f4c3e741427ef12f55aaddbf8322822c3fe43a86
Colin Gaffney <cpgaffney@google.com>
2022-11-09 13:16:49 -0800

    Increase orbax.checkpoint version.
    PiperOrigin-RevId: 487329098

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open('README.md') as fp:
 
 setuptools.setup(
     name='orbax',
-    version='0.0.14',
+    version='0.0.15',
     description='Orbax',
     long_description=_LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
