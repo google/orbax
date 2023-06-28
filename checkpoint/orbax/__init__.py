afc4b61c193d6a3b45f20521a37f25d2f11e3357
Colin Gaffney <cpgaffney@google.com>
2023-06-28 16:18:20 -0700

    Increase orbax.checkpoint version.
    PiperOrigin-RevId: 544189715

--- checkpoint/orbax/__init__.py
+++ checkpoint/orbax/__init__.py
@@ -59,4 +59,4 @@ if _in_ipython_session:
 PyTreeCheckpointer = functools.partial(Checkpointer, PyTreeCheckpointHandler())
 
 # A new PyPI release will be pushed everytime `__version__` is increased.
-__version__ = '0.2.6'
+__version__ = '0.2.7'
