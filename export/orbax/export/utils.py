9844d4a39c1ef808146540bed1243d7e66d19f70
Dinghua Li <dinghua@google.com>
2023-07-27 10:39:54 -0700

    Add a util function for auto batching optional inputs to match the batch size of primary inputs.
    PiperOrigin-RevId: 551575601

--- export/orbax/export/utils.py
+++ export/orbax/export/utils.py
@@ -17,7 +17,7 @@ from collections.abc import Mapping, Sequence
 import dataclasses
 import functools
 import inspect
-from typing import Any, Callable
+from typing import Any, Callable, Optional
 import jax
 import tensorflow as tf
 
@@ -74,7 +74,7 @@ def remove_signature_defaults(input_signature: PyTree) -> PyTree:
   )
 
 
-def _get_defaults(input_signature: PyTree) -> list[PyTree]:
+def _get_defaults(input_signature: Sequence[PyTree]) -> list[PyTree]:
   """Returns a list of default values corresponding with each parameter."""
   default_values = []
   for parameter in input_signature:
@@ -143,6 +143,115 @@ def with_default_args(
       jit_compile=False,
       autograph=False,
   )
+
+
+def _runtime_batch_size(x: tf.Tensor) -> tf.Tensor:
+  """Gets the runtime batch size of a tensor."""
+  return tf.shape(x)[0]
+
+
+@tf.function(autograph=True)
+def _repeat_to_batch(
+    x: tf.Tensor,
+    primary_batch_size: tf.Tensor,
+    tensor_name: Optional[str] = None,
+) -> tf.Tensor:
+  """Repeats a tensor to match a primary batch size."""
+  input_batch_size = _runtime_batch_size(x)
+  if input_batch_size == 1:
+    x = tf.repeat(x, primary_batch_size, axis=0)
+  else:
+    tf.assert_equal(
+        input_batch_size,
+        primary_batch_size,
+        f'The batch size of a non-primary input tensor (name={tensor_name})'
+        ' must be 1 or the same as that of the primary tensors.',
+    )
+  return x
+
+
+def make_auto_batching_function(
+    input_signature: Sequence[PyTree],
+) -> Callable[..., Any]:
+  """Creates an auto-batching function from input signature.
+
+  An auto-batching function is a function whose input tensors can have either
+  a batch size of "b" or 1, and whose output tensors have a batch size of "b",
+  where "b" is the batch size of the primary input tensors.
+
+  Requirements:
+    - All input tensors must have a leading batch dimension.
+    - There must be at least one primary tensor. A primary tensor is a tensor
+    whose tensor spec is either a `tf.TensorSpec` or a `TensorSpecWithDefault`
+    whose
+    is_primary attribute is True.
+    - All primary tensors must have the same batch size.
+    - All non-primary tensors must have a batch size of 1, or the same as the
+    primary batch size.
+
+  Example:
+    >>> input_signature = (
+    >>>     tf.TensorSpec([None], tf.int32, name='primary'),
+    >>>     TensorSpecWithDefault(
+    >>>         tf.TensorSpec([None], tf.int32, name='optional'), [1]
+    >>>     ),
+    >>> )
+    >>> batching_fn = utils.make_auto_batching_function(input_signature)
+    >>> batching_fn(tf.constant([0, 0]), tf.constant([1]))
+    (<tf.Tensor: shape=(2,), dtype=int32, numpy=array([0, 0], dtype=int32)>,
+    <tf.Tensor: shape=(2,), dtype=int32, numpy=array([1, 1], dtype=int32)>)
+
+  Args:
+    input_signature: a sequence of PyTrees whose leaf node is `tf.Tensor` or
+      `TensorSpecWithDefault`.
+
+  Returns:
+    A TF function whose output tensors all have the same batch size.
+  """
+  flat_sig, sig_treedef = jax.tree_util.tree_flatten(tuple(input_signature))
+  is_primary_tensor = [
+      isinstance(x, tf.TensorSpec) or x.is_primary for x in flat_sig
+  ]
+  tensor_names = [
+      x.name if isinstance(x, tf.TensorSpec) else x.tensor_spec.name
+      for x in flat_sig
+  ]
+
+  if not any(is_primary_tensor):
+    raise ValueError(
+        'No primary input tensors. A primary tensor is a tensor whose tensor'
+        ' spec is either a `tf.TensorSpec` or a `TensorSpecWithDefault` whose'
+        ' `is_primary` attribute is True. Got'
+        f' input_signature={input_signature}`'
+    )
+
+  def auto_batching_fn(*args):
+    flat_args, arg_treedef = jax.tree_util.tree_flatten(args)
+    assert arg_treedef == sig_treedef, (arg_treedef, sig_treedef)
+
+    primary_batch_size = None
+    for tensor, is_primary in zip(flat_args, is_primary_tensor):
+      if is_primary:
+        primary_batch_size = _runtime_batch_size(tensor)
+        break
+    assert primary_batch_size is not None
+
+    batched = []
+    for tensor, is_primary, name in zip(
+        flat_args, is_primary_tensor, tensor_names
+    ):
+      if is_primary:
+        tf.assert_equal(
+            primary_batch_size,
+            _runtime_batch_size(tensor),
+            'All primary input tensors must have the same batch size.',
+        )
+      else:
+        tensor = _repeat_to_batch(tensor, primary_batch_size, name)
+      batched.append(tensor)
+    return jax.tree_util.tree_unflatten(arg_treedef, batched)
+
+  return with_default_args(auto_batching_fn, input_signature)
 
 
 class CallableSignatures:
--- export/orbax/export/utils_test.py
+++ export/orbax/export/utils_test.py
@@ -106,6 +106,96 @@ class UtilsTest(tf.test.TestCase):
     loaded = utils.CallableSignatures.from_saved_model(export_dir, ['serve'])
     self.assertAllEqual(loaded.signatures['f'](), {'output': 2})
 
+  def test_make_auto_batching_function_simple(self):
+    input_signature = (
+        tf.TensorSpec([None], tf.int32, name='primary'),
+        TensorSpecWithDefault(
+            tf.TensorSpec([None], tf.int32, name='optional'), [1]
+        ),
+    )
+    batching_fn = utils.make_auto_batching_function(input_signature)
+    self.assertAllEqual(
+        batching_fn(tf.constant([0, 0]), tf.constant([1])),
+        (np.array([0, 0]), np.array([1, 1])),
+    )
+    self.assertAllEqual(
+        batching_fn(tf.constant([0, 0]), tf.constant([1, 2])),
+        (np.array([0, 0]), np.array([1, 2])),
+    )
+
+  def test_make_auto_batching_function_nested(self):
+    input_signature = (
+        tf.TensorSpec([None], tf.int32, name='primary_1'),
+        TensorSpecWithDefault(
+            tf.TensorSpec([None], tf.int32, name='primary_2'),
+            default_val=[1],
+            is_primary=True,
+        ),
+        {
+            'optional_1': TensorSpecWithDefault(
+                tf.TensorSpec([None], tf.int32, name='optional_1'),
+                default_val=[2],
+            ),
+            'optional_2': TensorSpecWithDefault(
+                tf.TensorSpec([None], tf.int32, name='optional_1'),
+                default_val=[3],
+            ),
+        },
+    )
+    tf_fn = utils.make_auto_batching_function(input_signature)
+
+    tf.nest.map_structure(
+        self.assertAllEqual,
+        # Non-primary inputs use default values: batch size = 1.
+        tf_fn([0, 0], [1, 1]),
+        (
+            np.array([0, 0]),
+            np.array([1, 1]),
+            {'optional_1': np.array([2, 2]), 'optional_2': np.array([3, 3])},
+        ),
+    )
+
+    tf.nest.map_structure(
+        self.assertAllEqual,
+        # Non-primary inputs batch size = 2.
+        tf_fn([0, 0], [1, 1], {'optional_1': [2, 2], 'optional_2': [3, 3]}),
+        (
+            np.array([0, 0]),
+            np.array([1, 1]),
+            {'optional_1': np.array([2, 2]), 'optional_2': np.array([3, 3])},
+        ),
+    )
+
+  def test_make_auto_batching_function_error(self):
+    input_signature_no_primary = (
+        TensorSpecWithDefault(tf.TensorSpec([None], tf.int32), default_val=[1]),
+    )
+    with self.assertRaisesRegex(ValueError, 'No primary input tensors'):
+      utils.make_auto_batching_function(input_signature_no_primary)
+
+  def test_make_auto_batching_function_runtime_error(self):
+    input_signature = (
+        tf.TensorSpec([None], tf.int32, name='primary_1'),
+        tf.TensorSpec([None], tf.int32, name='primary_2'),
+        TensorSpecWithDefault(
+            tf.TensorSpec([None], tf.int32, 'optional_1'), default_val=[1]
+        ),
+    )
+    batching_fn = utils.make_auto_batching_function(input_signature)
+
+    with self.assertRaisesRegex(
+        tf.errors.InvalidArgumentError,
+        'All primary input tensors must have the same batch size',
+    ):
+      batching_fn([1, 2], [3])
+
+    with self.assertRaisesRegex(
+        tf.errors.InvalidArgumentError,
+        'The batch size .* must be 1 or the same as that of the primary'
+        ' tensors',
+    ):
+      batching_fn([1, 2, 3], [4, 5, 6], [7, 8])
+
 
 if __name__ == '__main__':
   tf.test.main()
