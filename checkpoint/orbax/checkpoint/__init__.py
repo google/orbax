3ebe564b36cd6e818dba8c1abcaec750a4cbbf89
Colin Gaffney <cpgaffney@google.com>
2023-07-27 10:39:52 -0700

    Release.
    PiperOrigin-RevId: 551575586

--- checkpoint/orbax/checkpoint/__init__.py
+++ checkpoint/orbax/checkpoint/__init__.py
@@ -63,4 +63,4 @@ if _in_ipython_session:
 PyTreeCheckpointer = functools.partial(Checkpointer, PyTreeCheckpointHandler())
 
 # A new PyPI release will be pushed everytime `__version__` is increased.
-__version__ = '0.3.0'
+__version__ = '0.3.1'
