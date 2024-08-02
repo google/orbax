cbd9d575a842f73ef5a41973d67dc6a7fbff9fd0
Colin Gaffney <cpgaffney@google.com>
2024-08-01 17:16:46 -0700

    Internal change.
    PiperOrigin-RevId: 658591021

--- checkpoint/orbax/checkpoint/standard_checkpoint_handler_test_utils.py
+++ checkpoint/orbax/checkpoint/standard_checkpoint_handler_test_utils.py
@@ -218,11 +218,13 @@ class StandardCheckpointHandlerTestBase:
       )
       test_utils.assert_tree_equal(self, params, restored)
 
-    def test_empty_pytrees(self):
+    def test_empty_error(self):
       """Test case."""
       with self.assertRaises(ValueError):
         self.handler.save(self.directory, args=self.save_args_cls({}))
 
+    def test_empty_dict_node(self):
+      """Test case."""
       item = {'a': {}, 'b': 3}
       self.handler.save(self.directory, args=self.save_args_cls(item))
       restored = self.handler.restore(
@@ -230,6 +232,8 @@ class StandardCheckpointHandlerTestBase:
       )
       self.assertDictEqual(restored, item)
 
+    def test_empty_none_node(self):
+      """Test case."""
       item = {'c': None, 'd': 2}
       self.handler.save(self.directory, args=self.save_args_cls(item))
       restored = self.handler.restore(
