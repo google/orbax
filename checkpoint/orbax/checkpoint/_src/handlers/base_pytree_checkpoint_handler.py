b705528ff0e426563256d691119b597a36e937dc
Orbax Authors <no-reply@google.com>
2024-10-31 09:20:11 -0700

    Fix a bug in Orbax checkpointing where None values PyTree are not handled correctly. This is caused by a recent update in jax where None values are no longer considered as a leaf node: https://jax.readthedocs.io/en/latest/changelog.html#jax-0-4-34-october-4-2024.
    PiperOrigin-RevId: 691828986

--- checkpoint/orbax/checkpoint/_src/handlers/base_pytree_checkpoint_handler.py
+++ checkpoint/orbax/checkpoint/_src/handlers/base_pytree_checkpoint_handler.py
@@ -178,6 +178,10 @@ def batched_serialization_requests(
   ):
     nonlocal grouped
     tuple_key = tree_utils.tuple_path_from_keypath(keypath)
+
+    if info is None or arg is None or value is None:
+      return
+
     if info.skip_deserialize:
       return
 
@@ -226,6 +230,7 @@ def batched_serialization_requests(
       param_infos,
       tree,
       args,
+      is_leaf=lambda x: x is None,
   )
   return list(grouped.values())
 
