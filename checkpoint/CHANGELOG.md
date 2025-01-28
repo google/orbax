5f2133d982e77e28119e5365c5ed051e9978b9d6
Colin Gaffney <cpgaffney@google.com>
2025-01-28 15:07:54 -0800

    Increase version to 0.11.1, largely to incorporate change disabling features that introduced D2H regressions.
    PiperOrigin-RevId: 720722323

--- checkpoint/CHANGELOG.md
+++ checkpoint/CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.11.1] - 2025-01-28
+
 ### Changed
 
 - Add user-facing `TreeMetadata` object returned by
--- checkpoint/orbax/checkpoint/version.py
+++ checkpoint/orbax/checkpoint/version.py
@@ -16,7 +16,7 @@
 
 # A new PyPI release will be pushed everytime `__version__` is increased.
 # Also modify version and date in CHANGELOG.
-__version__ = '0.11.0'
+__version__ = '0.11.1'
 
 
 # TODO: b/362813406 - Add latest change timestamp and commit number.
