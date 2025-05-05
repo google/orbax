eb5fbd2d7c3f5cb4c5bfd99059594667e7ebb68f
Adam Cogdell <adamcogdell@google.com>
2025-05-05 09:35:08 -0700

    Fix v1 options docstrings for readthedocs.
    PiperOrigin-RevId: 754967288

--- checkpoint/orbax/checkpoint/experimental/v1/_src/context/options.py
+++ checkpoint/orbax/checkpoint/experimental/v1/_src/context/options.py
@@ -33,11 +33,12 @@ from orbax.checkpoint.experimental.v1._src.tree import types as tree_types
 class AsyncOptions:
   """Options used to configure async behavior.
 
-  timeout_secs: The timeout in seconds for the async save operation.
-  post_finalization_callback: A function that is called after the async save
-    operation is complete.
-  create_directories_asynchronously: If true, create directories asynchronously
-    in the background.
+  timeout_secs:
+    The timeout in seconds for the async save operation.
+  post_finalization_callback:
+    A function that is called after the async save operation is complete.
+  create_directories_asynchronously:
+    If true, create directories asynchronously in the background.
   """
 
   timeout_secs: int = 600  # 10 minutes.
@@ -56,19 +57,20 @@ class AsyncOptions:
 class MultiprocessingOptions:
   """Options used to configure multiprocessing behavior.
 
-  primary_host: the host id of the primary host.  Default to 0.  If it's set
-    to None, then all hosts will be considered as primary.  It's useful in
-    the case that all hosts are only working with local storage.
-  active_processes: A set of process indices (corresponding to
-    `multihost.process_index()`) over which `CheckpointManager` is expected to
-    be called. This makes it possible to have a `CheckpointManager` instance
-    that runs over a subset of processes, rather than all processes as it is
-    normally expected to do. If specified, `primary_host` must belong to
-    `active_processes`.
-  barrier_sync_key_prefix: A string to be prepended to the barrier sync key
-    used to synchronize processes. This is useful to avoid collisions with
-    other barrier syncs if another CheckpointManager is being used
-    concurrently.
+  primary_host:
+    The host id of the primary host.  Default to 0.  If it's set to None, then
+    all hosts will be considered as primary.  It's useful in the case that all
+    hosts are only working with local storage.
+  active_processes:
+    A set of process indices (corresponding to `multihost.process_index()`) over
+    which `CheckpointManager` is expected to be called. This makes it possible
+    to have a `CheckpointManager` instance that runs over a subset of processes,
+    rather than all processes as it is normally expected to do. If specified,
+    `primary_host` must belong to `active_processes`.
+  barrier_sync_key_prefix:
+    A string to be prepended to the barrier sync key used to synchronize
+    processes. This is useful to avoid collisions with other barrier syncs if
+    another CheckpointManager is being used concurrently.
   """
 
   primary_host: int | None = 0
@@ -89,12 +91,15 @@ class FileOptions:
   """Options used to configure checkpoint directories and files.
 
   Attributes:
-    path_permission_mode: Path permission mode for step directories, user
-      metadata files. e.g. 0o750. Please check
+    path_permission_mode:
+      Path permission mode for step directories, user metadata files. e.g.
+      0o750. Please check
       https://github.com/google/etils/blob/main/etils/epath/backend.py if your
-        path is supported. default=None.
-    temporary_path_class: A class that is used to create and finallize temporary
-      paths, and to ensure atomicity.
+      path is supported. default=None.
+    temporary_path_class:
+      A class that is used to create and finallize temporary paths, and to
+      ensure atomicity.
+
   """
 
   path_permission_mode: int | None = None
@@ -123,9 +128,10 @@ class PyTreeOptions:
   class Saving:
     """Options for saving PyTrees.
 
-    create_array_storage_options_fn: A function that is called in order to
-      create `ArrayOptions.Saving.StorageOptions` for each leaf in a PyTree,
-      when it is being saved. It is called similar to:
+    create_array_storage_options_fn:
+      A function that is called in order to create
+      `ArrayOptions.Saving.StorageOptions` for each leaf in a PyTree, when it is
+      being saved. It is called similar to:
       `jax.tree.map_with_path(create_array_storage_options_fn, pytree_to_save)`.
       If provided, it overrides any default settings in
       `ArrayOptions.Saving.StorageOptions`.
@@ -182,8 +188,8 @@ class ArrayOptions:
       use_ocdbt: Enables OCDBT format.
       use_zarr3: If True, use Zarr3 format.
       ocdbt_target_data_file_size: Specifies the target size (in bytes) of each
-        OCDBT data file.  It only applies when OCDBT is enabled and Zarr3 must
-        be turned on.  If left unspecified, default size is 2GB.  A value of 0
+        OCDBT data file. It only applies when OCDBT is enabled and Zarr3 must be
+        turned on. If left unspecified, default size is 2GB.  A value of 0
         indicates no maximum file size limit.  For best results, ensure
         chunk_byte_size is smaller than this value.  For more details, refer to
         https://google.github.io/tensorstore/kvstore/ocdbt/index.html#json-kvstore/ocdbt.target_data_file_size
@@ -204,17 +210,15 @@ class ArrayOptions:
         jnp.bfloat16 is not compatible with np.ndarray).
       chunk_byte_size:
         This is an experimental feature that automatically chooses the largest
-        chunk
-        shape possible, while keeping the chunk byte size less than or equal
-        to the
-        specified chunk_byte_size. Both the write_chunk_shape and
-        read_chunk_shape
-        are automatically set to the chosen shape. This uses a greedy
-        algorithm that
-        prioritizes splitting the largest dimensions first.
-      shard_axes: An optional list of axes that should be prioritized when
-        sharding array for storage. If empty, storage sharding implementation
-        will prioritize axes which are already sharded.
+        chunk shape possible, while keeping the chunk byte size less than or
+        equal to the specified chunk_byte_size. Both the write_chunk_shape and
+        read_chunk_shape are automatically set to the chosen shape. This uses a
+        greedy algorithm that prioritizes splitting the largest dimensions
+        first.
+      shard_axes:
+        An optional list of axes that should be prioritized when sharding array
+        for storage. If empty, storage sharding implementation will prioritize
+        axes which are already sharded.
       """
 
       dtype: np.typing.DTypeLike | None = None
@@ -235,12 +239,13 @@ class ArrayOptions:
   class Loading:
     """Options for loading arrays.
 
-    concurrent_bytes: Max concurrent bytes that are allowed for reading. Can
-      help to reduce the possibility of OOM's when large checkpoints are
-      restored.
-    enable_padding_and_truncation: If True, restoration allows silent
-      truncating/padding of arrays if the stored array shape does not match the
-      target shape. Otherwise, raises an error.
+    concurrent_bytes:
+      Max concurrent bytes that are allowed for reading. Can help to reduce the
+      possibility of OOM's when large checkpoints are restored.
+    enable_padding_and_truncation:
+      If True, restoration allows silent truncating/padding of arrays if the
+      stored array shape does not match the target shape. Otherwise, raises an
+      error.
     """
     concurrent_bytes: int | None = None
     enable_padding_and_truncation: bool = False
