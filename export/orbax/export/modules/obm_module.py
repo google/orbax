9c130b3dea191119de5ad915fedf352048486f01
Orbax Authors <no-reply@google.com>
2025-05-19 16:19:50 -0700

    minor fix on description
    PiperOrigin-RevId: 760810985

--- export/orbax/export/modules/obm_module.py
+++ export/orbax/export/modules/obm_module.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Wraps JAX functions and parameters into a tf.Module."""
+"""Wraps JAX functions and parameters into a ObmModule(Orbax Model Module)."""
 
 from collections.abc import Callable, Mapping, Sequence
 import copy
