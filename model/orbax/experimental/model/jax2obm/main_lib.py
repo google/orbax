1d6ec3e362bf53734c573a0a2e4bad1e05fb91e6
Orbax Authors <no-reply@google.com>
2025-05-22 16:54:51 -0700

    No public description
    PiperOrigin-RevId: 762180265

--- model/orbax/experimental/model/jax2obm/main_lib.py
+++ model/orbax/experimental/model/jax2obm/main_lib.py
@@ -97,8 +97,6 @@ def jax_exported_to_shlo_fn(
   return shlo_func
 
 
-# TODO(wangpeng): Rename to `convert_to_obm_fn` or `convert_with_spec` or
-#   `convert_with_signature`.
 def convert(
     fun_jax: Callable[..., utils.JaxArrayPyTree],
     args_spec: Sequence[Any],
