db9035fce93b62acf3f4d73ad4d8149cdf2e2748
Orbax Authors <no-reply@google.com>
2025-06-12 00:01:06 -0700

    Update logic for applying the default LatestN preservation policy in CM
    PiperOrigin-RevId: 770505058

--- checkpoint/orbax/checkpoint/checkpoint_manager.py
+++ checkpoint/orbax/checkpoint/checkpoint_manager.py
@@ -231,7 +231,7 @@ def _get_default_preservation_policy(
             n=options.max_to_keep,
         )
     )
-  else:
+  elif options.max_to_keep is not None or not preservation_policies:
     preservation_policies.append(
         preservation_policy_lib.LatestN(n=options.max_to_keep)
     )
