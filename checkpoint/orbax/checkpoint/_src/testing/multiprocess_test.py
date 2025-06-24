30d6ee652412018273db21b497beb3f500e96386
Michael Whittaker <mwhittaker@google.com>
2025-06-24 11:27:26 -0700

    Remove old heartbeat flags and arguments.
    PiperOrigin-RevId: 775315340

--- checkpoint/orbax/checkpoint/_src/testing/multiprocess_test.py
+++ checkpoint/orbax/checkpoint/_src/testing/multiprocess_test.py
@@ -162,8 +162,6 @@ def _main(argv):
         f"--jax_task_id={i}",
         f"--jax_controller_address=localhost:{jax_port}",
         "--jax_heartbeat_timeout=3s",
-        "--jax_heartbeat_interval=1s",
-        "--jax_max_missing_heartbeats=3",
         f"--jax_distributed_shutdown_timeout={_WORKER_SHUTDOWN_TIMEOUT.value}s",
         "--vmodule=client=10,service=10",
     ]
