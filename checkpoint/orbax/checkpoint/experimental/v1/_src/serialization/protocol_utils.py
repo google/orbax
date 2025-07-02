637c67c82d203ad51ab389df020e5a118c9a0856
Daniel Ng <dnlng@google.com>
2025-07-02 14:19:20 -0700

    V1: Protocol util to be used for Leaf Handler Registry
    PiperOrigin-RevId: 778640035

---
+++ checkpoint/orbax/checkpoint/experimental/v1/_src/serialization/protocol_utils.py
@@ -0,0 +1,62 @@
+# Copyright 2025 The Orbax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utility to work with Protocol."""
+
+from typing import Any, Type
+
+from absl import logging
+import typing_extensions
+
+
+def is_subclass_protocol(cls: Type[Any], protocol: Type[Any]) -> bool:
+  """Perform a best-effort check if `cls` is a subclass of protocol.
+
+  This is needed because when the protocol has only attributes (no methods),
+  isinstance() doesn't work.  In additional, issubclass also doesn't work with
+  Protocols.  This is a best-effort workaround and can only check if the `cls`
+  defines all the attributes of the `protocol`.  It does't check the type of the
+  attributes.
+
+  Args:
+    cls: the cls will be checked if it implements `protocol`.
+    protocol: the protocol to be checked.
+
+  Returns:
+    True if the class defines all the attributes of the protocol.
+  """
+
+  if not typing_extensions.is_protocol(protocol):  # pytype: disable=not-supported-yet
+    raise ValueError(f'Protocol {protocol} is not a Protocol.')
+
+  members = typing_extensions.get_protocol_members(protocol)  # pytype: disable=not-supported-yet
+
+  if members and not hasattr(cls, '__annotations__'):
+    # generic types such as int
+    return False
+
+  for attr in members:
+    logging.vlog(1, 'attribute: %s', attr)
+
+    # check if the attribute is defined in annotations or members
+    if attr not in cls.__annotations__ and not hasattr(cls, attr):
+      logging.vlog(
+          1,
+          'no matching attribute: %s, cls[%s] has %s',
+          attr,
+          cls.__name__,
+          dir(cls),
+      )
+      return False
+  return True
---
+++ checkpoint/orbax/checkpoint/experimental/v1/_src/serialization/protocol_utils_test.py
@@ -0,0 +1,164 @@
+# Copyright 2025 The Orbax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unittests for protocol_utils."""
+
+import dataclasses
+from typing import Protocol, Sequence
+from absl.testing import absltest
+import jax
+import numpy as np
+from orbax.checkpoint.experimental.v1._src.serialization import protocol_utils
+
+
+class ProtocolUtilsTest(absltest.TestCase):
+
+  def test_is_define_protocol_attributes_simple(self):
+    class AbstractProtocolAttr(Protocol):
+      attr1: int
+      attr2: float
+
+    @dataclasses.dataclass
+    class Child1:
+      attr1: int
+      attr2: float
+
+    c = Child1(1, 11)
+
+    self.assertTrue(
+        protocol_utils.is_subclass_protocol(type(c), AbstractProtocolAttr)
+    )
+
+  def test_is_define_protocol_attributes_with_extra_attributes(self):
+    class AbstractProtocolAttr(Protocol):
+      attr1: int
+      attr2: float
+
+    class Child1:
+      attr1: int
+      attr2: float
+      attr3: str
+      attr4: Sequence[int]
+
+    self.assertTrue(
+        protocol_utils.is_subclass_protocol(Child1, AbstractProtocolAttr)
+    )
+
+  def test_is_define_protocol_attributes_not_match(self):
+    class AbstractProtocolAttr(Protocol):
+      attr1: int
+      attr2: float
+
+    class Child1:
+      attr1: int
+      attr3: float
+
+    self.assertFalse(
+        protocol_utils.is_subclass_protocol(Child1, AbstractProtocolAttr)
+    )
+
+    self.assertFalse(
+        protocol_utils.is_subclass_protocol(int, AbstractProtocolAttr)
+    )
+
+    self.assertFalse(
+        protocol_utils.is_subclass_protocol(np.float32, AbstractProtocolAttr)
+    )
+
+  def test_with_methods(self):
+    class AbstractProtocolAttr(Protocol):
+      attr1: int
+      attr2: float
+
+      def foo(self):
+        ...
+
+    class ChildWithFoo:
+      attr1: int
+      attr2: float
+
+      def foo(self):
+        pass
+
+    class ChildWithOutFoo:
+      attr1: int
+      attr2: float
+
+      def other_foo(self):
+        pass
+
+    self.assertTrue(
+        protocol_utils.is_subclass_protocol(ChildWithFoo, AbstractProtocolAttr)
+    )
+    self.assertFalse(
+        protocol_utils.is_subclass_protocol(
+            ChildWithOutFoo, AbstractProtocolAttr
+        )
+    )
+
+  def test_exception_for_non_protocol(self):
+    with self.assertRaisesRegex(ValueError, "Protocol .* is not a Protocol"):
+      protocol_utils.is_subclass_protocol(int, int)
+
+  def test_complex_type(self):
+    class AbstractProtocol(Protocol):
+      attr1: jax.Array
+      attr2: np.ndarray
+
+    class Child(AbstractProtocol):
+      attr1: jax.Array
+      attr2: np.ndarray
+
+    self.assertTrue(
+        protocol_utils.is_subclass_protocol(Child, AbstractProtocol)
+    )
+
+  def test_with_property(self):
+    class AbstractProtocol(Protocol):
+      attr1: jax.Array
+
+      @property
+      def foo(self) -> int:
+        ...
+
+    @dataclasses.dataclass
+    class ChildWithFoo:
+      attr1: jax.Array
+      foo: int
+
+    self.assertTrue(
+        protocol_utils.is_subclass_protocol(ChildWithFoo, AbstractProtocol)
+    )
+
+    @dataclasses.dataclass
+    class ChildWithoutFoo:
+      attr1: jax.Array
+      not_foo: int
+
+    self.assertFalse(
+        protocol_utils.is_subclass_protocol(ChildWithoutFoo, AbstractProtocol)
+    )
+
+  def test_same_protocol(self):
+    class AbstractProtocol(Protocol):
+      attr1: jax.Array
+      attr2: int
+
+    self.assertTrue(
+        protocol_utils.is_subclass_protocol(AbstractProtocol, AbstractProtocol)
+    )
+
+
+if __name__ == "__main__":
+  absltest.main()
