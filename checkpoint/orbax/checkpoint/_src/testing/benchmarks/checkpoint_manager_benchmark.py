08d72a0bb804b8d1ba7c2aa8a53bf7ef63602377
Orbax Authors <no-reply@google.com>
2025-10-02 22:55:55 -0700

    Add a benchmark for `orbax.checkpoint.CheckpointManager`.
    PiperOrigin-RevId: 814546232

---
+++ checkpoint/orbax/checkpoint/_src/testing/benchmarks/checkpoint_manager_benchmark.py
@@ -0,0 +1,120 @@
+# Copyright 2025 The Orbax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Benchmarks for orbax.checkpoint.checkpoint_manager."""
+
+import dataclasses
+from typing import Any, Sequence
+
+from absl import logging
+import jax
+import numpy as np
+from orbax.checkpoint import args as args_lib
+from orbax.checkpoint import checkpoint_manager
+from orbax.checkpoint import utils
+from orbax.checkpoint._src.testing.benchmarks.core import core as benchmarks_core
+from orbax.checkpoint._src.testing.benchmarks.core import pytree_utils
+
+
+@dataclasses.dataclass(frozen=True)
+class CheckpointManagerBenchmarkOptions(benchmarks_core.BenchmarkOptions):
+  """Configuration options for benchmarks targeting CheckpointManager."""
+
+  save_interval_steps: int | Sequence[int] = 1
+  max_to_keep: int | Sequence[int] = 1
+  train_steps: int | Sequence[int] = 1
+
+
+@benchmarks_core.benchmark_options(CheckpointManagerBenchmarkOptions)
+class CheckpointManagerBenchmark(benchmarks_core.BenchmarksGenerator):
+  """A benchmark for orbax.checkpoint.CheckpointManager."""
+
+  def _get_pytree_for_restore(self, pytree: Any) -> Any:
+    """Returns abstract pytree for restore."""
+    return jax.tree.map(utils.to_shape_dtype_struct, pytree)
+
+  # TODO(nikhilbansall): Expand to full training loop with multiple steps,
+  # including a step of training.
+  def test_fn(
+      self, context: benchmarks_core.TestContext
+  ) -> benchmarks_core.TestResult:
+    metrics = benchmarks_core.Metrics()
+    pytree = context.pytree
+    options = context.options
+    assert isinstance(options, CheckpointManagerBenchmarkOptions)
+
+    cm_options = checkpoint_manager.CheckpointManagerOptions(
+        save_interval_steps=options.save_interval_steps,
+        max_to_keep=options.max_to_keep,
+    )
+    mngr = checkpoint_manager.CheckpointManager(
+        context.path, options=cm_options
+    )
+
+    json_data = {'a': 1, 'b': 'test'}
+    random_key = jax.random.key(0)
+    np_random_key = np.random.get_state()
+
+    composite_args = args_lib.Composite(
+        pytree=args_lib.StandardSave(pytree),
+        json_item=args_lib.JsonSave(json_data),
+        jax_random_key=args_lib.JaxRandomKeySave(random_key),
+        np_random_key=args_lib.NumpyRandomKeySave(np_random_key),
+    )
+
+    step_saved = -1
+    for step in range(options.train_steps):
+      logging.info('Saving checkpoint at step %d', step)
+      with metrics.time(f'save_{step}'):
+        if mngr.save(step, args=composite_args):
+          step_saved = step
+      with metrics.time(f'wait_until_finished_{step}'):
+        mngr.wait_until_finished()
+      logging.info('Finished saving checkpoint at step %d', step)
+
+    if step_saved == -1:
+      raise AssertionError('No checkpoint was saved.')
+
+    pytree_for_restore = self._get_pytree_for_restore(pytree)
+
+    restore_args = args_lib.Composite(
+        pytree=args_lib.StandardRestore(pytree_for_restore),
+        json_item=args_lib.JsonRestore(),
+        jax_random_key=args_lib.JaxRandomKeyRestore(),
+        np_random_key=args_lib.NumpyRandomKeyRestore(),
+    )
+    latest_step = mngr.latest_step()
+    assert latest_step == step_saved, (
+        f'Expected latest step to be {step_saved}, got {latest_step}'
+    )
+
+    with metrics.time(f'restore_{latest_step}'):
+      logging.info('Restoring checkpoint at step %d', latest_step)
+      restored = mngr.restore(latest_step, args=restore_args)
+      logging.info('Finished restoring checkpoint at step %d', latest_step)
+
+    with metrics.time('correctness_check'):
+      pytree_utils.assert_pytree_equal(pytree, restored['pytree'])
+      assert (
+          json_data == restored['json_item']
+      ), f"Expected {json_data}, got {restored['json_item']}"
+      assert jax.numpy.array_equal(
+          random_key, restored['jax_random_key']
+      ), f"Expected {random_key}, got {restored['jax_random_key']}"
+      jax.tree.map(
+          np.testing.assert_equal, np_random_key, restored['np_random_key']
+      )
+
+    mngr.close()
+    return benchmarks_core.TestResult(metrics=metrics)
---
+++ checkpoint/orbax/checkpoint/_src/testing/benchmarks/checkpoint_manager_benchmark_test.py
@@ -0,0 +1,128 @@
+# Copyright 2025 The Orbax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from unittest import mock
+
+from absl.testing import absltest
+from absl.testing import parameterized
+from etils import epath
+import jax
+import jax.numpy as jnp
+import numpy as np
+from orbax.checkpoint import checkpoint_manager
+from orbax.checkpoint._src.testing.benchmarks import checkpoint_manager_benchmark
+from orbax.checkpoint._src.testing.benchmarks.core import configs as benchmarks_configs
+from orbax.checkpoint._src.testing.benchmarks.core import core as benchmarks_core
+
+
+CheckpointManagerBenchmarkOptions = (
+    checkpoint_manager_benchmark.CheckpointManagerBenchmarkOptions
+)
+CheckpointManagerBenchmark = (
+    checkpoint_manager_benchmark.CheckpointManagerBenchmark
+)
+
+
+class CheckpointManagerBenchmarkTest(parameterized.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.mock_checkpoint_manager_cls = self.enter_context(
+        mock.patch.object(
+            checkpoint_manager, 'CheckpointManager', autospec=True
+        )
+    )
+
+  @parameterized.parameters(
+      dict(
+          options=CheckpointManagerBenchmarkOptions(train_steps=1),
+          expected_len=1,
+      ),
+      dict(
+          options=CheckpointManagerBenchmarkOptions(
+              train_steps=[1, 2], max_to_keep=2
+          ),
+          expected_len=2,
+      ),
+      dict(
+          options=CheckpointManagerBenchmarkOptions(
+              train_steps=1, max_to_keep=[1, 2]
+          ),
+          expected_len=2,
+      ),
+  )
+  def test_generate_benchmarks(self, options, expected_len):
+    generator = CheckpointManagerBenchmark(
+        checkpoint_config=benchmarks_configs.CheckpointConfig(spec={}),
+        options=options,
+    )
+    benchmarks = generator.generate()
+    self.assertLen(benchmarks, expected_len)
+    for benchmark in benchmarks:
+      self.assertIsInstance(
+          benchmark.options, CheckpointManagerBenchmarkOptions
+      )
+
+  def test_benchmark_test_fn_succeeds(self):
+    generator = CheckpointManagerBenchmark(
+        checkpoint_config=benchmarks_configs.CheckpointConfig(spec={}),
+        options=CheckpointManagerBenchmarkOptions(),
+    )
+    mock_cm_instance = self.mock_checkpoint_manager_cls.return_value
+    mock_cm_instance.should_save.return_value = True
+    mock_cm_instance.latest_step.return_value = 0
+
+    # Mock return of restore
+    pytree = {
+        'a': jnp.arange(10),
+    }
+    json_data = {'a': 1, 'b': 'test'}
+    random_key = jax.random.key(0)
+    np_random_key = np.random.get_state()
+
+    mock_cm_instance.restore.return_value = {
+        'pytree': pytree,
+        'json_item': json_data,
+        'jax_random_key': random_key,
+        'np_random_key': np_random_key,
+    }
+
+    test_path = epath.Path(self.create_tempdir().full_path)
+    test_options = CheckpointManagerBenchmarkOptions(train_steps=1)
+    context = benchmarks_core.TestContext(
+        pytree=pytree, path=test_path, options=test_options
+    )
+
+    result = generator.test_fn(context)
+
+    self.mock_checkpoint_manager_cls.assert_called_once()
+    self.assertIsInstance(result, benchmarks_core.TestResult)
+    self.assertContainsSubset(
+        {'save_0', 'wait_until_finished_0', 'restore_0', 'correctness_check'},
+        result.metrics.timings.keys(),
+    )
+    mock_cm_instance.save.assert_called_once()
+    mock_cm_instance.wait_until_finished.assert_called_once()
+    mock_cm_instance.restore.assert_called_once()
+    mock_cm_instance.close.assert_called_once()
+
+  def test_options_class(self):
+    self.assertEqual(
+        CheckpointManagerBenchmarkOptions,
+        CheckpointManagerBenchmark.options_class,
+    )
+
+
+if __name__ == '__main__':
+  absltest.main()
