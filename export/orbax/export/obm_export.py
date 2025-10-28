bb6c493b167a527cf2d1fb44735306e1eef4ba66
He Jiang <hjswe@google.com>
2025-10-28 10:25:01 -0700

    Get the pipeline_named_signature from obm functions.
    PiperOrigin-RevId: 825100419

--- export/orbax/export/obm_export.py
+++ export/orbax/export/obm_export.py
@@ -31,7 +31,6 @@ from orbax.export import jax_module
 from orbax.export import obm_configs
 from orbax.export import oex_orchestration
 from orbax.export import serving_config as osc
-from orbax.export import typing
 from orbax.export import utils
 from orbax.export.modules import obm_module
 import tensorflow as tf
