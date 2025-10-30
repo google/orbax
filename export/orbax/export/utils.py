1ddc39774f1e67cc7b84c1132db883ec7b34b7fb
He Jiang <hjswe@google.com>
2025-10-30 07:53:45 -0700

    Internal change
    PiperOrigin-RevId: 826030630

--- export/orbax/export/utils.py
+++ export/orbax/export/utils.py
@@ -26,10 +26,12 @@ import jax
 from jax import export as jax_export
 from jax import tree_util
 import jaxtyping
-# pylint: disable-next=g-direct-tensorflow-import
+import numpy as np
+
 from orbax.export import constants
 from orbax.export import serving_config as osc
 import tensorflow as tf
+# pylint: disable-next=g-direct-tensorflow-import
 
 
 ConfigProto = Any
